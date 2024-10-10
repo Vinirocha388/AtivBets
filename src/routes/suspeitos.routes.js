f82fdacf832e251637612b5b8506084190defaa8
Vinícius Rocha <vinicius.a.rocha8@aluno.br>
2024-10-10 16:24:07 -0300

    fix: bug fix

--- src/routes/suspeitos.routes.js
+++ src/routes/suspeitos.routes.js
@@ -2,24 +2,24 @@ import { Router } from "express";
 
 const suspeitosRoutes = Router();
 
-// Array com candidatos pré-cadastrados
+// Array com suspeitos pré-cadastrados
 let suspeitos = [
   {
-    id: Math.floor(Math.random() * 1000),
+    id: Math.floor(Math.random() * 10),
     nome: "Capitã Lucimara",
     profissão: "Capitã da PM",
     envolvimento: "Sim",
     nivelSuspeito: "Baixo"
   },
   {
-    id: Math.floor(Math.random() * 1000),
+    id: Math.floor(Math.random() * 10),
     nome: "Pablo Marçal",
     profissão: "Quase prefeito de SP",
     envolvimento: "Não",
     nivelSuspeito: "Médio"
   },
   {
-    id: Math.floor(Math.random() * 1000),
+    id: Math.floor(Math.random() * 10),
     nome: "Bombeiro Zé",
     profissão: "Vereador",
     envolvimento: "Não",
@@ -29,19 +29,19 @@ let suspeitos = [
   
 ];
 
-// Rota para listar todos os candidatos
+// Rota para listar todos os suspeitos
 suspeitosRoutes.get("/", (req, res) => {
   return res.status(200).json(suspeitos);
 });
 
-// Rota para cadastrar um novo candidato
+// Rota para cadastrar um novo suspeito
 suspeitosRoutes.post("/", (req, res) => {
     const { nome, profissao, envolvimento, nivelSuspeito } = req.body;
 
-  // Validação dos campos nome e partido
-  if (!nome || !profissao || !nivelSuspeito) {
+  // Validação dos campos nome, profissão,nível de suspeito e envolvimento
+  if (!nome || !profissao || !nivelSuspeito || !envolvimento) {
     return res.status(400).send({
-      message: "O nome, profissão ou nivel de suspeito não foi preenchido, Por favor preencha !!",
+      message: "O nome, profissão, nivel de suspeito ou envolvimento não foi preenchido, Por favor preencha !!",
     });
   }
 
@@ -51,10 +51,13 @@ suspeitosRoutes.post("/", (req, res) => {
   const novoSuspeito = {
     id: Math.floor(Math.random() * 1000),
     nome,
+    profissao,
+    envolvimento,
+    nivelSuspeito
     
   };
 
-  // Adiciona o novo candidato ao array de candidatos
+  // Adiciona o novo suspeito ao array de suspeitos
   suspeitos.push(novoSuspeito);
 
   return res.status(201).json({
@@ -64,7 +67,7 @@ suspeitosRoutes.post("/", (req, res) => {
 });
 
 // Rota para buscar um suspeito pelo id
-suspeitostosRoutes.get("/:id", (req, res) => {
+suspeitosRoutes.get("/:id", (req, res) => {
   const { id } = req.params;
 
   // Busca um suspeito pelo id no array
@@ -95,17 +98,12 @@ suspeitosRoutes.put("/:id", (req, res) => {
       .json({ message: `Suspeito com id ${id} não encontrado!` });
   }
 
-  // Validação dos campos envolvimento e níveld e suspeita
-  if (!envolvimento || !nivelSuspeito  ) {
-    return res.status(400).send({
-      message: "O envolvimento ou o nível de suspeita não foi preenchido, Por favor preencha!!",
-    });
-  }
+
 
   suspeito.nome = nome;
   suspeito.profissao = profissao;
   suspeito.envolvimento = envolvimento;
-  suspeito.nivelSuspeito = NivelSuspeito;
+  suspeito.nivelSuspeito = nivelSuspeito;
 
   return res.status(200).json({
     message: "Suspeito atualizado com sucesso!",
--- src/server.js
+++ src/server.js
@@ -2,11 +2,13 @@ import express from "express";
 import { config } from "dotenv";
 
 config();
+import routes from "./routes/index.routes.js";
 
 const serverPort = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.json());
+app.use(routes);
 
 app.listen(serverPort, () => {
   console.log(`⚡ Servidor top rodando no http://localhost:${serverPort}`);
